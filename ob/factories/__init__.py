e5f60b11cd5f7a2ed3dafecaac2a5c3d022709d8
nanvel <1374878+nanvel@users.noreply.github.com>
2022-10-29 13:21:37 +0700

    updates factory

--- ob/factories/__init__.py
+++ ob/factories/__init__.py
@@ -1,6 +1,7 @@
 from .order_book import OrderBookFactory
+from .order_book_updates import OrderBookUpdatesFactory
 from .symbol import SymbolFactory
 from .time import TimeFactory
 
 
-__all__ = ("OrderBookFactory", "SymbolFactory", "TimeFactory")
+__all__ = ("OrderBookFactory", "OrderBookUpdatesFactory", "SymbolFactory", "TimeFactory")
