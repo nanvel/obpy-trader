d06b607f7f95efbced54b509b68f90ca7672deb9
nanvel <1374878+nanvel@users.noreply.github.com>
2022-10-30 18:34:23 +0700

    policy for ddb

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -7,9 +7,6 @@ const stack = pulumi.getStack();
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("obpy");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
-
 const obpyHandlerRole = new aws.iam.Role("obpyHandlerRole", {
   assumeRolePolicy: {
     Version: "2012-10-17",
@@ -25,44 +22,6 @@ const obpyHandlerRole = new aws.iam.Role("obpyHandlerRole", {
     ],
   },
 });
-
-const obpyHandlerPolicy = new aws.iam.Policy("obpyHandlerPolicy", {
-  policy: {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Effect: "Allow",
-        Action: [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-        ],
-        Resource: "*",
-      },
-      {
-        Action: ["s3:GetObject"],
-        Effect: "Allow",
-        Resource: [pulumi.interpolate`${bucket.arn}/*`],
-      },
-    ],
-  },
-});
-
-new aws.iam.PolicyAttachment("obpyHandlerRoleAttachment", {
-  roles: [obpyHandlerRole.name],
-  policyArn: obpyHandlerPolicy.arn,
-});
-
-const obpyHandlerFunc = new aws.lambda.Function("obpyHandlerFunc", {
-  code: new pulumi.asset.AssetArchive({
-    ".": new pulumi.asset.FileArchive("./app"),
-  }),
-  runtime: "python3.9",
-  handler: "lambda_function.lambda_handler",
-  role: obpyHandlerRole.arn,
-});
-
-bucket.onObjectCreated("obpyHandler", obpyHandlerFunc);
 
 const obpyTable = new aws.dynamodb.Table("obpyTable", {
   attributes: [
@@ -86,4 +45,48 @@ const obpyTable = new aws.dynamodb.Table("obpyTable", {
   writeCapacity: 0,
 });
 
-export const obpyTableName = obpyTable.id;
+const obpyHandlerPolicy = new aws.iam.Policy("obpyHandlerPolicy", {
+  policy: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Resource: "*",
+      },
+      {
+        Action: ["s3:GetObject"],
+        Effect: "Allow",
+        Resource: [pulumi.interpolate`${bucket.arn}/*`],
+      },
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:PutItem"],
+        Resource: pulumi.interpolate`${obpyTable.arn}`,
+      },
+    ],
+  },
+});
+
+new aws.iam.PolicyAttachment("obpyHandlerRoleAttachment", {
+  roles: [obpyHandlerRole.name],
+  policyArn: obpyHandlerPolicy.arn,
+});
+
+const obpyHandlerFunc = new aws.lambda.Function("obpyHandlerFunc", {
+  code: new pulumi.asset.AssetArchive({
+    ".": new pulumi.asset.FileArchive("./app"),
+  }),
+  runtime: "python3.9",
+  handler: "lambda_function.lambda_handler",
+  role: obpyHandlerRole.arn,
+});
+
+bucket.onObjectCreated("obpyHandler", obpyHandlerFunc);
+
+export const bucketName = bucket.id;
+export const tableName = obpyTable.id;
