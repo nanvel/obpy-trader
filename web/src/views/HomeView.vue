c448b117dab4c7147f4407e6c9128c1119607131
nanvel <1374878+nanvel@users.noreply.github.com>
2022-11-06 17:49:19 +0700

    display files

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+import axios from "axios";
+import { NDataTable } from "naive-ui";
+
+const files = ref(null);
+
+type ObpyFile = {
+  Exchange: string;
+  Symbol: string;
+  Path: string;
+};
+
+const columns = [
+  {
+    title: "Exchange",
+    key: "Exchange",
+  },
+  {
+    title: "Symbol",
+    key: "Symbol",
+  },
+  {
+    title: "Key",
+    key: "Key",
+  },
+];
+
+axios
+  .get("/api/cloud/files")
+  .then((data) => (files.value = data["data"]["data"]));
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <n-data-table :data="files" :columns="columns" v-if="files" />
   </main>
 </template>
